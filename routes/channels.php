8841d8dcb5228140a2595d1acd9ac55dcb1e8859
kareem22T <m.othman@clicksegypt.net>
2024-07-28 08:51:16 +0300

    edits

--- routes/channels.php
+++ routes/channels.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (float) $user->id === (float) $id;
+    return (int) $user->id === (int) $id;
 });
